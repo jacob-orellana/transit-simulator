ad0d56cb004a4833a9f42a2e1b084b7757dc76e7
SE User <cse@se-vm.local>
2018-10-03 15:21:25 -0500

    Added final test cases for project tests.

--- unit_tests/test_undirected_graph.js
+++ unit_tests/test_undirected_graph.js
@@ -167,3 +167,33 @@ QUnit.test('fifth weighted graph test case for projections', (assert) => {
   graph.addEdge('s', new UndirectedEdge(4), 't');
   assert.deepEqual(shortestUndirectedPath(graph, 's', (vertex) => vertex === 't'), 't\'', undefined);
 });
+
+QUnit.test('sixth weighted graph test case for projections', (assert) => {
+  const graph = new UndirectedGraph();
+  graph.addVertex('s');
+  graph.addVertex('t');
+  graph.addVertex('t\'');
+  graph.addVertex('c');
+  graph.addVertex('b');
+  graph.addEdge('s', new UndirectedEdge(1), 'c');
+  graph.addEdge('s', new UndirectedEdge(4), 'b');
+  graph.addEdge('c', new UndirectedEdge(2), 't\'');
+  graph.addEdge('b', new UndirectedEdge(2), 't');
+
+  assert.deepEqual(shortestUndirectedPath(graph, 's', (vertex) => vertex === 't'), 't\'', undefined);
+});
+
+QUnit.test('seventh weighted graph test case for projections', (assert) => {
+  const graph = new UndirectedGraph();
+  graph.addVertex('s');
+  graph.addVertex('t');
+  graph.addVertex('t\'');
+  graph.addVertex('c');
+  graph.addVertex('u');
+  graph.addVertex('u\'');
+  graph.addEdge('c', new UndirectedEdge(2), 'u');
+  graph.addEdge('s', new UndirectedEdge(1), 'c');
+  graph.addEdge('s', new UndirectedEdge(4), 'u\'');
+  graph.addEdge('u\'', new UndirectedEdge(2), 't');
+  assert.deepEqual(shortestUndirectedPath(graph, 's', (vertex) => vertex === 't'), 't\'', undefined);
+});
