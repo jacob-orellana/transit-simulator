bd9c25d0c15b12fdf751ea98ab83c6d4e4eaa24d
SE User <cse@se-vm.local>
2018-10-11 15:22:10 -0500

    cleaned up the code a little bit by clearing log statements

--- js/undirected_graph.js
+++ js/undirected_graph.js
@@ -73,16 +73,12 @@ function shortestUndirectedPath(graph, source, destinationPredicate, projection 
   let destination = undefined;
   const backpointers = new Map();
   const priority = new Map();
-  // const distance = new Map();
   const worklist = new PriorityQueue((element) => priority.get(`${[element]}`));
   priority.set(`${[undefined, source]}`, 0 + heuristic(source, source.destination));
-  // distance.set(`${[undefined, source]}`, 0);
   worklist.enqueue([undefined, source]);
-  // console.log(source);
   while (worklist.elements !== []) {
     const [from, to] = worklist.dequeue();
     const prior = priority.get(`${[from, to]}`);
-    // const dist = distance.get(`${[from, to]}`);
     if (destinationPredicate(to)) {
       destination = to;
       backpointers.set(to, from);
@@ -94,22 +90,14 @@ function shortestUndirectedPath(graph, source, destinationPredicate, projection 
     backpointers.set(to, from);
     projections.push(projection(to));
     for (const neighbor of graph.getNeighbors(to)) {
-      // distance.set(`${[to, neighbor]}`, dist + 1);
       priority.set(`${[to, neighbor]}`, prior + graph.getEdge(to, neighbor).weight);
-      // console.log(neighbor);
       worklist.enqueue([to, neighbor]);
     }
   }
   const result = [];
-  // console.log(backpointers);
-  // console.log(destination);
   for (let current = destination; current !== undefined; current = backpointers.get(current)) {
-    // console.log('this');
     result.push(current);
-    // console.log(current);
   }
-  // console.log(result);
-  // console.log(destination);
   if (result === []) {
     return undefined;
   }
