bd6ce60266e264fc6093105a2552a71612308361
Adrian Pilkington <span style=font-style:italicpilkadrian6@gmail.com/span>
2018-12-05 14:45:15 -0600

    Finished hash table test cases.

--- unit_tests/test_hash_table.js
+++ unit_tests/test_hash_table.js
@@ -1,5 +1,5 @@
 QUnit.module('hash_table.js');
-/* globals QUnit HashTable */
+/* globals QUnit HashTable mod isPseudoPrime increaseToPseudoPrime createBuckets */
 /* eslint-disable no-magic-numbers */
 /* eslint no-underscore-dangle: 0 */
 
@@ -7,7 +7,7 @@ QUnit.module('hash_table.js');
 /*
   - You could have 7 buckets or more than 7 buckets.
   - The size of your hash table could  be 0 or greater than zero.
-  - The hashFunction you pass when your create a table can return the element or change the element.
+  - The hashFunction you pass when your create a table can return the element or change the element
 */
 
 // This test case works with a hash table that is empty, having 7 buckets, and
@@ -49,4 +49,46 @@ QUnit.test('test a resize', (assert) => {
   assert.deepEqual(testHash.get(13), 3);
   assert.deepEqual(testHash.delete(9), false);
   assert.deepEqual(testHash.delete(10), true);
+});
+
+// This test case tests different values sent to the mod function that the hash table uses
+QUnit.test('test the mod function', (assert) => {
+  // test when the value is smaller than the modulus
+  assert.deepEqual(mod(3, 5), 3);
+  // test when the value is larger than the modulus
+  assert.deepEqual(mod(5, 2), 1);
+  // test when result is < 0
+  assert.deepEqual(mod(-3, 5), 2);
+  assert.deepEqual(mod(-5, 2), 1);
+  // test with zeros
+  assert.deepEqual(mod(0, 5), 0);
+  assert.deepEqual(mod(5, 0), NaN);
+});
+
+// This test case tests different values sent to the isPseudoPrime function that the hash table uses
+QUnit.test('test the isPseudoPrime function', (assert) => {
+  // test base case
+  assert.deepEqual(isPseudoPrime(2), false);
+  assert.deepEqual(isPseudoPrime(8), false);
+  // test a prime number
+  assert.deepEqual(isPseudoPrime(7), true);
+  assert.deepEqual(isPseudoPrime(17), true);
+});
+
+// This test case tests different values sent to the increaseToPseudoPrime function that the hash table uses
+QUnit.test('test the increaseToPseudoPrime function', (assert) => {
+  // test increasing a number that is already prime
+  assert.deepEqual(increaseToPseudoPrime(17), 17);
+  assert.deepEqual(increaseToPseudoPrime(23), 23);
+  // test a number that is not prime number
+  assert.deepEqual(increaseToPseudoPrime(10), 11);
+  assert.deepEqual(increaseToPseudoPrime(30), 31);
+});
+
+// This test case tests different values sent to the createBuckets function that the hash table uses
+QUnit.test('test the createBuckets function', (assert) => {
+  // test a count of zero
+  assert.deepEqual(createBuckets(0), []);
+  // test a count of more than zero
+  assert.deepEqual(createBuckets(3), [[], [], []]);
 });
