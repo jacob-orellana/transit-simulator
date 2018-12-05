7b1b35418d932f9f67e0084983f871638f67d28f
Adrian Pilkington <span style=font-style:italicpilkadrian6@gmail.com/span>
2018-12-05 13:27:29 -0600

    Working on test cases for hash table.

--- unit_tests/test_hash_table.js
+++ unit_tests/test_hash_table.js
@@ -1,6 +1,7 @@
 QUnit.module('hash_table.js');
 /* globals QUnit HashTable */
 /* eslint-disable no-magic-numbers */
+/* eslint no-underscore-dangle: 0 */
 
 // CATEGORY PARTITION METHOD
 /*
@@ -29,38 +30,31 @@ QUnit.test('test an empty has table', (assert) => {
 
 QUnit.test('test a resize', (assert) => {
   const testHash = new HashTable((element) => element + 4);
+  assert.deepEqual(testHash._buckets.length, 7);
   // put enough elements in the hash table to cause a resize
   testHash.set(1, 5);
   testHash.set(4, 1);
   testHash.set(10, 0);
   testHash.set(13, 3);
-  console.log(testHash);  
   testHash.set(21, 4);
-  console.log(testHash);
-  // check to see if elements have been moved according to the resize
-  assert.deepEqual(testHash.get(1), 5);
-  assert.deepEqual(testHash.get(4), 8);
-  assert.deepEqual(testHash.get(10), 14);
-  assert.deepEqual(testHash.get(13), 0);
-  assert.deepEqual(testHash.get(21), 8);
+  // check to see if the bucket has been resized and elements are in the right index
+  assert.deepEqual(testHash._buckets.length, 17);
+  assert.deepEqual(testHash._buckets[0], [[13, 3]]);
+  assert.deepEqual(testHash._buckets[14], [[10, 0]]);
+  assert.deepEqual(testHash._buckets[8], [[4, 1], [21, 4]]);
 });
 
-// QUnit.test('test elements in the same bucket', (assert) => {
-//   const testHash = new HashTable(() => 4);
-//   testHash.set(4, 1);
-//   testHash.set(4, 4);
-//   testHash.set(4, 10);
-//   testHash.set(4, 13);
-//   testHash.set(4, 21);
-//   // test methods when multiple numbers are in one buckets
-//   console.log(testHash.size);
-//   assert.deepEqual(testHash._buckets[4], [
-//     [
-//       4,
-//       21,
-//     ],
-// ]);
-//   assert.deepEqual(testHash.has(4), true);
-//   assert.deepEqual(testHash.get(4), 9);
-//   assert.deepEqual(testHash.delete(4), true);
-// });
+QUnit.test('test elements in the same bucket', (assert) => {
+  const testHash = new HashTable((element) => element);
+  testHash.set(4, 1);
+  // testHash.set(4, 4);
+  // testHash.set(4, 10);
+  // testHash.set(4, 13);
+  // testHash.set(4, 21);
+  // test methods when multiple numbers are in one buckets
+  console.log(testHash._buckets);
+  assert.deepEqual(testHash._buckets[4], [[4, 21]]);
+  assert.deepEqual(testHash.has(4), true);
+  assert.deepEqual(testHash.get(4), 9);
+  assert.deepEqual(testHash.delete(4), true);
+});
